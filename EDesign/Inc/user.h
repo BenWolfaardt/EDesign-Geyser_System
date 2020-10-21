9f39f63087213b8d8bfe119f518fdb5706cb2944
BenWolfaardt <35372441+BenWolfaardt@users.noreply.github.com>
2020-10-21 20:25:15 +0200

    Version 2.1

--- EDesign/Inc/user.h
+++ EDesign/Inc/user.h
@@ -24,6 +24,7 @@ void DecodeCmd(void);
 void UserInitialise(void);
 void User(void);
 void resetAll(void);
+void writeToPins(uint8_t segments[], uint8_t pins[], int segmentsL);
 
 uint8_t Int2String(char* outputString, int16_t value, uint8_t maxL);
 uint8_t String2Int(char* inputString, int16_t* outputInt);
--- EDesign/Src/user.c
+++ EDesign/Src/user.c
@@ -13,12 +13,17 @@
 #define maxTxL  50  		// maximum length of transmit buffer (replies sent back to UART)
 
 volatile bool uartRxFlag;	// use 'volatile' keyword because the variable is changed from interrupt handler
+volatile bool systickFlag;
 
 char uartRxChar;			// temporary storage
 char cmdBuf[cmdBufL];  		// buffer in which to store commands that are received from the UART
 char txBuf[maxTxL]; 		// buffer for replies that are to be sent out on UART
 
 char* txStudentNo = "$A,18321933\r\n";
+
+uint8_t numberMap[10];
+uint8_t pinsValue[4];
+uint8_t segements[4];
 
 int16_t tempSetpoint;		// the current temperature set point
 
@@ -27,13 +32,36 @@ uint16_t cmdBufPos;  		// this is the position in the cmdB where we are currentl
 void UserInitialise(void)
 {
 	uartRxFlag = false;
+	systickFlag = false;
 	tempSetpoint = 60;		// initial value
+
+	numberMap[0] = 0b00111111;
+	numberMap[1] = 0b00000110;
+	numberMap[2] = 0b01011011;
+	numberMap[3] = 0b01001111;
+	numberMap[4] = 0b01100110;
+	numberMap[5] = 0b01101101;
+	numberMap[6] = 0b01111101;
+	numberMap[7] = 0b00000111;
+	numberMap[8] = 0b01111111;
+	numberMap[9] = 0b01100111;
+
+	segements[0] = 0b0001;
+	segements[1] = 0b0010;
+	segements[2] = 0b0100;
+	segements[3] = 0b1000;
+
+	pinsValue[0] = numberMap[1];
+	pinsValue[1] = numberMap[9];
+	pinsValue[2] = numberMap[5];
+	pinsValue[3] = numberMap[2];
 
 	HAL_UART_Receive_IT(&huart1, (uint8_t*)&uartRxChar, 1);	// UART interrupt after 1 character was received
 }
 
 void User(void)
 {
+
 	if (uartRxFlag)
 	{
 		if (uartRxChar == '$')
@@ -50,6 +78,12 @@ void User(void)
 		}
 		uartRxFlag = false;  // clear the flag - the 'receive character' event has been handled.
 		HAL_UART_Receive_IT(&huart1, (uint8_t*)&uartRxChar, 1);	// UART interrupt after 1 character was received
+	}
+	if(systickFlag == 1U)
+	{
+		systickFlag = 0U;
+
+		writeToPins(segements, pinsValue, 4);
 	}
 }
 
@@ -85,63 +119,40 @@ void DecodeCmd()
 		HAL_UART_Transmit(&huart1, (uint8_t*)txBuf, charsL+5, 1000);
 		break;
 
-	case 'T':
-	  	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_10,GPIO_PIN_SET); //1
-	  	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_SET); //2
-	  	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET); //3
-		//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,GPIO_PIN_SET); //4
-
-
-
-
-//		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, 1); //a
-//		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6, 1); //b
-//		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7, 1); //c
-//		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 1); //d
-//		HAL_GPIO_WritePin(GPIOC,GPIO_PIN_7, 1); //e
-//		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8, 1); //f
-//		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9, 1); //g
+	case 'R':
+		resetAll();
 
 		break;
 	}
 }
 
-// HAL_UART_RxCpltCallback - callback function that will be called from the UART interrupt handler.
-// This function will execute whenever a character is received from the UART
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart)
-{
-	// the interrupt handler will automatically put the received character in the uartRXChar variable (no need to write any code for that).
-	// so all we do it set flag to indicate character was received, and then process the received character further in the main loop
-	uartRxFlag = true;
-}
-
 uint8_t String2Int(char* inputString, int16_t* outputInt)
 {
-    int returnValue = 0;
-    int sign = 1;
+	int returnValue = 0;
+	int sign = 1;
 
-    if (*inputString == '\0')
-        return 0;
+	if (*inputString == '\0')
+		return 0;
 
-    if (*inputString == '-')
-    {
-        sign = -1;
-        inputString++;
-    }
+	if (*inputString == '-')
+	{
+		sign = -1;
+		inputString++;
+	}
 
-    while ((*inputString >= '0') && (*inputString <= '9'))
-    {
-    	returnValue *= 10;
-    	returnValue += (*inputString - 48);
+	while ((*inputString >= '0') && (*inputString <= '9'))
+	{
+		returnValue *= 10;
+		returnValue += (*inputString - 48);
 
-        if (((sign == 1) && (returnValue >= 32768)) ||
-            ((sign == -1) && (returnValue >= 32769)))
-            return 0;
+		if (((sign == 1) && (returnValue >= 32768)) ||
+				((sign == -1) && (returnValue >= 32769)))
+			return 0;
 
-        inputString++;
-    }
-    *outputInt = (int16_t)(sign * returnValue);
-    return 1;
+		inputString++;
+	}
+	*outputInt = (int16_t)(sign * returnValue);
+	return 1;
 }
 
 // convert integer var to ASCII string
@@ -191,11 +202,58 @@ uint8_t Int2String(char* outputString, int16_t value, uint8_t maxL)
 
 void resetAll(void)
 {
-	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET); //1
-	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET); //2
-	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET); //3
-	  HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_RESET); //4
-	  HAL_GPIO_WritePin(GPIOC,GPIO_PIN_7,GPIO_PIN_RESET); //5
-	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_RESET); //6
-	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_RESET); //7
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET); //a
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET); //b
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET); //c
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_SET); //d
+	HAL_GPIO_WritePin(GPIOC,GPIO_PIN_7,GPIO_PIN_SET); //e
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_SET); //f
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_SET); //g
+}
+
+void writeToPins(uint8_t segments[], uint8_t pins[], int segmentsL)
+{
+	int i = 0;
+
+	while (i <= segmentsL)
+	{
+		//HAL_Delay(1);
+		//Use 1ms flag as HAL delay adjust timing of board and makes it either quicker or slower on eachcycle
+
+		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_10,(~(segements[i] >> 0) & 0b00000001)); //1
+		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,(~(segements[i] >> 1) & 0b00000001)); //2
+		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,(~(segements[i] >> 2) & 0b00000001)); //3
+		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,(~(segements[i] >> 3) & 0b00000001)); //4
+
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, (~(pins[i] >> 0) & 0b00000001)); //a
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6, (~(pins[i] >> 1) & 0b00000001)); //b
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7, (~(pins[i] >> 2) & 0b00000001)); //c
+		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, (~(pins[i] >> 3) & 0b00000001)); //d
+		HAL_GPIO_WritePin(GPIOC,GPIO_PIN_7, (~(pins[i] >> 4) & 0b00000001)); //e
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8, (~(pins[i] >> 5) & 0b00000001)); //f
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9, (~(pins[i] >> 6) & 0b00000001)); //g
+
+		i++;
+
+		if(i == 4)
+			i = 0;
+	}
+}
+
+//----------------------------------------------------------------------------------------------//
+//											Interupts											//
+//----------------------------------------------------------------------------------------------//
+
+// HAL_UART_RxCpltCallback - callback function that will be called from the UART interrupt handler.
+// This function will execute whenever a character is received from the UART
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart)
+{
+	// the interrupt handler will automatically put the received character in the uartRXChar variable (no need to write any code for that).
+	// so all we do it set flag to indicate character was received, and then process the received character further in the main loop
+	uartRxFlag = true;
+}
+
+void HAL_SYSTICK_Callback(void)
+{
+	systickFlag = true;
 }
